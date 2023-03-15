b0085026031f64bfe4be5a931059f227e2c579d0
PunitDh <cas012115@coderacademy.edu.au>
2023-03-15 23:08:50 +1100

    Update error handler for comments

--- routers/comments.js
+++ routers/comments.js
@@ -6,72 +6,71 @@ const comments = Router();
 
 comments.use(csrf());
 
-comments.get("/", async (req, res) => {
+comments.get("/", async (req, res, next) => {
   try {
     const comments = await Comment.all();
     return res.render("comments/index", { comments });
-  } catch (e) {}
+  } catch (e) {
+    next(e)
+  }
 });
 
-comments.get("/new", async (req, res) => {
+comments.get("/new", async (req, res, next) => {
   try {
     const blogs = await Blog.all();
     const comment = new Comment();
     return res.render("comments/new", { comment, blogs });
   } catch (e) {
-    req.flash("error", e.message);
-    return res.redirect("/comments");
+    next(e)
   }
 });
 
-comments.get("/edit/:id", async (req, res) => {
+comments.get("/edit/:id", async (req, res, next) => {
   try {
     const comment = await Comment.find(req.params.id);
     const blogs = await Blog.all();
     return res.render("comments/edit", { comment, blogs });
   } catch (e) {
-    req.flash("error", e.message);
+    next(e)
   }
 });
 
-comments.post("/edit/:id", async (req, res) => {
+comments.post("/edit/:id", async (req, res, next) => {
   try {
     await Comment.update(req.params.id, req.body);
     req.flash("success", "Comment has been updated");
     return res.redirect(`/comments`);
   } catch (e) {
-    req.flash("error", e.message);
-    return res.redirect(`/comments/edit/${req.params.id}`);
+    next(e)
   }
 });
 
-comments.post("/delete/:id", async (req, res) => {
+comments.post("/delete/:id", async (req, res, next) => {
   try {
     await Comment.delete(req.params.id);
     req.flash("success", "Comment has been deleted");
     return res.redirect("/comments");
   } catch (e) {
-    req.flash("error", e.message);
+    next(e)
   }
 });
 
-comments.get("/:id", async (req, res) => {
+comments.get("/:id", async (req, res, next) => {
   try {
     const comment = await Comment.find(req.params.id);
     return res.render("comments/comment", { comment });
   } catch (e) {
-    req.flash("error", e.message);
+    next(e)
   }
 });
 
-comments.post("/new", async (req, res) => {
+comments.post("/new", async (req, res, next) => {
   try {
     await Comment.create(req.body);
     req.flash("success", "Comment has been added");
     return res.redirect(`/comments`);
   } catch (e) {
-    req.flash("error", e.message);
-    return res.redirect("comments/new");
+    next(e)
   }
 });
 
